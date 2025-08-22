9e901af0dbeaa4388373fb73b12ea0f3a8a937c0
Ghirda Melania <118004482+gmelania@users.noreply.github.com>
2025-08-22 11:33:29 +0000

    progress 04_04b

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import chef from "./images/chef.jpg";
 
@@ -9,36 +10,34 @@ function Header({ name, year }) {
     </header>
   );
 }
+
 const items = [
   "Macaroni and Cheese",
   "Salmon with Potatoes",
   "Tofu with Vegetables",
-  "Minestrone Soup"
+  "Minestrone Soup",
 ];
 
 const dishObjects = items.map((dish, i) => ({
   id: i,
-  title: dish
+  title: dish,
 }));
 
-function Main({ dishes }) {
+function Main({ dishes, openStatus, onStatus }) {
   return (
     <>
       <div>
         <h2>Welcome to this beautiful restaurant!</h2>
+        <p>
+          English: At the moment we are {openStatus == true ? "open" : "closed"}
+        </p>
+        <button onClick={() => onStatus(true)}>Request open</button>
       </div>
       <main>
-        <img
-          src={chef}
-          height={200}
-          alt="A photo of a smiling chef owner"
-        />
+        <img src={chef} height={200} alt="A photo of a smiling chef owner" />
         <ul>
           {dishes.map((dish) => (
-            <li
-              key={dish.id}
-              style={{ listStyleType: "none" }}
-            >
+            <li key={dish.id} style={{ listStyleType: "none" }}>
               {dish.title}
             </li>
           ))}
@@ -49,10 +48,19 @@ function Main({ dishes }) {
 }
 
 function App() {
+  const [status, setStatus] = useState(false);
+
   return (
     <div>
+      <h2>
+        Cafe di Melania e {status == false ? "chiuso" : "aperto"} al momento.
+      </h2>
+      <button onClick={() => setStatus(!status)}>
+        {status ? "Chiudere" : "Aprire"} il Cafe
+      </button>
+
       <Header name="Alex" year={new Date().getFullYear()} />
-      <Main dishes={dishObjects} />
+      <Main dishes={dishObjects} openStatus={status} onStatus={setStatus} />
     </div>
   );
 }
