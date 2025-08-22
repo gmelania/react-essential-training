03c7258f86ea39ee4cb425fdf260f8a37224caa1
Ghirda Melania <118004482+gmelania@users.noreply.github.com>
2025-08-22 18:31:45 +0000

    progress 06_01-02b

--- next-react-project/src/app/hotels/page.js
+++ next-react-project/src/app/hotels/page.js
@@ -1,8 +1,33 @@
-export default function Page() {
+async function getData() {
+  const res = await fetch("https://snowtooth-hotel-api.fly.dev");
+  return res.json();
+}
+
+// dymanic component to render inside the page
+function HotelBlock({ name, capacity }) {
+  return (
+    <div>
+      <h2>{name}</h2>
+      <p>{capacity}</p>
+    </div>
+  );
+}
+
+export default async function Page() {
+  const data = await getData();
   return (
     <main>
       <div>
-        <h1>Hotel Details</h1>
+        <h1>Hotels:</h1>
+        <div>
+          {data.map((hotel) => (
+            <HotelBlock
+              key={hotel.id}
+              name={hotel.name}
+              capacity={hotel.capacity}
+            />
+          ))}
+        </div>
       </div>
     </main>
   );
--- next-react-project/src/app/mountain/page.js
+++ next-react-project/src/app/mountain/page.js
@@ -1,7 +1,32 @@
-export default function Page() {
+async function getData() {
+  const response = await fetch("https://snowtooth-api-rest.fly.dev");
+  return response.json();
+}
+
+export default async function Page() {
+  // fetch data in the page
+  const data = await getData();
   return (
     <main>
       <h1>Lift Status Info</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Lift Name</th>
+            <th>Current Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            data.map((lift) => (
+              <tr key={lift.id}>
+                <td>{lift.name}</td>
+                <td>{lift.status}</td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
     </main>
   );
 }
